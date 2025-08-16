cbb76008e6fff75d1c576c66d6769a51f355967c
slimeking200 <ethangreyz@gmail.com>
2025-08-15 20:25:43 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,35 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('nfc-cache').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.webmanifest'
-      ]);
-    })
-  );
+// Cache-first app shell
+const CACHE = 'nfc-editor-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './service-worker.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(hit => hit || fetch(req).then(resp => {
+      if (req.method === 'GET' && resp.ok) {
+        const clone = resp.clone();
+        caches.open(CACHE).then(c => c.put(req, clone));
+      }
+      return resp;
+    }).catch(() => caches.match('./index.html')))
   );
 });
